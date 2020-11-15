3a8746e677207fc668d53d77650e8ee4654f82b3
sytnic <sytnic@list.ru>
2020-11-15 15:43:50 +0300

    Version instead 4.0. Difference from 3.0 only in file content/accord.php

---
+++ content/html/thislevel.php
@@ -0,0 +1,20 @@
+<?php
+
+$level = '../../'; 
+
+require_once($level.'includes/all.php');
+
+// итемы левого сайдбара в этой папке
+$sidebar = array(                            								
+	'index.php'        => 'Html-Главная',
+    'html_intro.php'   => 'Html-Введение',					 
+	'html_basic.php'   => 'Html-Основы',
+);
+
+// работает для пагинации
+$numeric_sidebar = array(                            								
+	'index.php',
+    'html_intro.php',					 
+	'html_basic.php'
+);
+
